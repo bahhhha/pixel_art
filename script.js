56c28dce515ff36b25d331057e5195b8075e1430
mintjulep21 <zharylkassyn001@gmail.com>
2022-03-18 06:04:01 +0600

    Finished HTML and CSS. Script needs further improvement and optimization

--- script.js
+++ script.js
@@ -1,15 +1,69 @@
 const canvas =  document.getElementById("canvas");
-var color = "black";
+const btn_clear = document.getElementById("clear");
+const btn_erase = document.getElementById("erase");
+var eraseron = false;
+const changesize = document.getElementById("changesize");
+const size_show = document.getElementById("size_show");
+var length = changesize.value; 
+var mouseDown = 0;
+document.body.onmousedown = function() { 
+  mouseDown = 1;
+}
+document.body.click = function() {
+    mouseDown = 1;
+}
+document.body.onmouseup = function() {
+  mouseDown = 0;
+}
+size_show.innerHTML = `${length} x ${length}`;
+
+changesize.addEventListener("change", function adjust_size() {
+    length = changesize.value;
+    size_show.innerHTML = `${length} x ${length}`;
+    createcanvas(length);
+});
+
+
+
+btn_clear.addEventListener("click", function clearcanvas() {
+    const nodeList = document.querySelectorAll(".box");
+    for (let i = 0; i < nodeList.length; i++) {
+        nodeList[i].style.backgroundColor = "whitesmoke";
+    }
+}
+);
+
+btn_erase.addEventListener("click", function switch_eraser() {
+    if (eraseron) {
+        eraseron = false;
+        btn_erase.style.backgroundColor = "whitesmoke";
+    }
+    else {
+        eraseron = true;
+        btn_erase.style.backgroundColor = "#d2fe90";
+    }
+    
+});
+
 function createcanvas(length) {
     canvas.innerHTML = '';
-    for (var i = 0; i < length * length; i++) {
+    for (let i = 0; i < length * length; i++) {
     const box = document.createElement("div");
-    box.addEventListener("mousedown", function changecolor() {
-        box.style.backgroundColor =  `${color}`;
+   
+    box.addEventListener("mousemove", function changecolor() {
+        if (mouseDown === 1) {
+            if (eraseron) {
+            box.style.backgroundColor = "whitesmoke";
+        }
+            else {
+            box.style.backgroundColor =  `${document.getElementById("favcolor").value}`;
+        }
+    }
     });
+    
     box.style.flex = `0 0 ${canvas.clientWidth / 16 / length}em`;
     box.classList.add("box");
     canvas.appendChild(box);
  }
 }
-createcanvas(16);
+createcanvas(length);
